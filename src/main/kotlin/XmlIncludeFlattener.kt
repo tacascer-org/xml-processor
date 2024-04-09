3a5eb6ef9f7165f035eb2c2146101d484a29dd72
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-04-09 18:42:22 -0400

    feat: Can recursively include schemas

--- src/main/kotlin/XmlIncludeFlattener.kt
+++ src/main/kotlin/XmlIncludeFlattener.kt
@@ -1,6 +1,7 @@
 package io.github.tacascer
 
 import io.github.oshai.kotlinlogging.KotlinLogging
+import org.jdom2.Document
 import org.jdom2.Element
 import org.jdom2.filter.Filters
 import org.jdom2.input.SAXBuilder
@@ -35,27 +36,28 @@ class XmlIncludeFlattener(private val input: Path, format: Format = Format.getPr
      * @throws Exception if an error occurs during processing
      */
     fun process(): String {
-        doProcess()
+        val output = process(inputDocument)
         return StringWriter().use {
-            outputter.output(inputDocument, it)
+            outputter.output(output, it)
             it.toString()
         }
     }
 
-    private fun doProcess() {
-        inputDocument.getDescendants(Filters.element()).filter { it.name == "include" }.forEach {
-            it.detach()
-            inlineInclude(it)
-        }
+    private fun process(inputDocument: Document): Document {
+        val output = inputDocument.clone()
+        val includeElements = output.getDescendants(Filters.element()).filter { it.name == "include" }
+        includeElements.forEach(Element::detach)
+        includeElements.forEach { inline(it, output) }
+        return output
     }
 
-    private fun inlineInclude(include: Element) {
+    private fun inline(include: Element, output: Document) {
         val includedSchema = include.getAttributeValue("schemaLocation")
         logger.info { "Processing included schema: $includedSchema" }
         val includedDocument = saxBuilder.build(URI(includedSchema).toPath().inputStream())
-        includedDocument.rootElement.getDescendants(Filters.element()).toList().forEach {
-            it.detach()
-            inputDocument.rootElement.addContent(it)
-        }
+        val inlinedDocument = process(includedDocument)
+        val inlinedElements = inlinedDocument.rootElement.getDescendants(Filters.element()).toList()
+        inlinedElements.forEach(Element::detach)
+        inlinedElements.forEach<Element?>(output.rootElement::addContent)
     }
 }
