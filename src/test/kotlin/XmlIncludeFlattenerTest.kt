d9031a647bad7a39d341fa03b5800fd9e148fa2d
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-04-11 11:57:08 -0400

    feat: output to given file

--- src/test/kotlin/XmlIncludeFlattenerTest.kt
+++ src/test/kotlin/XmlIncludeFlattenerTest.kt
@@ -1,6 +1,7 @@
 import io.github.tacascer.XmlIncludeFlattener
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.engine.spec.tempdir
+import io.kotest.engine.spec.tempfile
 import io.kotest.matchers.shouldBe
 import org.intellij.lang.annotations.Language
 import kotlin.io.path.createFile
@@ -38,6 +39,29 @@ class XmlIncludeFlattenerTest : FunSpec({
             includedFile.writeText(includedFileText)
 
             XmlIncludeFlattener(includingFile).process().trimIndent() shouldBe expectedText
+        }
+
+        test("given one xsd that includes another xsd through URI, when process(Path), should write out a single xsd with all elements") {
+            val outputFile = tempfile("output", ".xsd")
+            @Language("XML") val includingFileText = """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" targetNamespace="http://www.sample.com">
+                <xs:include schemaLocation="${testDir.toPath().resolve("sample_1.xsd").toUri()}"/>
+                <xs:element name="sample" type="xs:string"/>
+            </xs:schema>
+            """.trimIndent()
+            includingFile.writeText(includingFileText)
+            @Language("XML") val includedFileText = """
+            <?xml version="1.0" encoding="UTF-8"?>
+            <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+                <xs:element name="sampleOne" type="xs:string"/>
+            </xs:schema>
+            """.trimIndent()
+            includedFile.writeText(includedFileText)
+
+            XmlIncludeFlattener(includingFile).process(outputFile.toPath())
+
+            outputFile.readText().trimIndent() shouldBe expectedText
         }
 
         test("given one xsd that includes another in the same directory, should return a single xsd with all elements") {
